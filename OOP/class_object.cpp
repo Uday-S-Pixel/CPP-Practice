eff5b9afc736859df2bbd34d040cf1f2ac1ba0b0
Uday <suravarapuuday160@gmail.com>
2026-03-09 19:29:16 +0530

    Enhance class_object.cpp with explanatory comments
    Added comments to explain class functionality and usage.

--- OOP/class_object.cpp
+++ OOP/class_object.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 class Numprint
+// Class that stores a number and prints it
 {
     private:
+// private members can only be accessed inside the class
+// this helps protect data from being modified directly
     int a;
     
     public:
+//any method(functions) or variable mentioned under public can be used anywhere in the program
     void getdata()
     {
         cout<<"Enter the number u want to print"<<endl;
@@ -21,6 +25,7 @@ class Numprint
 int main()
 {
     Numprint d;
+    //d is an object to the class Numprint which allows access to the public members of the class
     d.getdata();
     d.output();
     
