555930c85d1b7a059b46a1fe48ca1d66007e8d08
Uday <suravarapuuday160@gmail.com>
2026-03-09 19:46:50 +0530

    Refactor Numprint class for improved readability
    Updated user prompts for clarity and added comments for better understanding.

--- OOP/class_object.cpp
+++ OOP/class_object.cpp
@@ -1,33 +1,40 @@
+/* Program to demonstrate a simple class and object in C++.
+   The class stores a number entered by the user and prints it.
+   Concepts used: class, object, private data member, public member functions.
+*/
+
 #include <iostream>
 using namespace std;
-class Numprint
+
 // Class that stores a number and prints it
+class Numprint
 {
-    private:
-// private members can only be accessed inside the class
-// this helps protect data from being modified directly
-    int a;
-    
-    public:
-//any method(functions) or variable mentioned under public can be used anywhere in the program
+private:
+    int a;   // private variable to store the number (cannot be accessed outside the class)
+
+public:
+    // function to take input from the user
     void getdata()
     {
-        cout<<"Enter the number u want to print"<<endl;
-        cin>>a;
+        cout << "Enter the number you want to print: ";
+        cin >> a;
     }
-    
+
+    // function to display the number
     void output()
     {
-        cout<<"The number is: "<<a;
+        cout << "The number is: " << a;
     }
 };
 
 int main()
 {
+    // creating an object of class Numprint
     Numprint d;
-    //d is an object to the class Numprint which allows access to the public members of the class
+
+    // calling member functions using the object
     d.getdata();
     d.output();
-    
+
     return 0;
 }
