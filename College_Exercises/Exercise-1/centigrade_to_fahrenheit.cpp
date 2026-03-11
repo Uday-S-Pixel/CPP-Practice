2e156dd0dfc2166249738a788325db9f00fb3134
Uday <suravarapuuday160@gmail.com>
2026-03-11 20:42:16 +0530

    Add centigrade to Fahrenheit conversion program

---
+++ College_Exercises/Exercise-1/centigrade_to_fahrenheit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float c,F;
+  cout<<"Enter temperature in centigrade: ";
+  cin>>c;
+  F=(c*1.8)+32;
+  cout<<"Temperature in Fahrenheit: "<<F<<"\n";
+  return 0;
+}
+
+
