b6969cb935dafe4a5ec21c1d1939807aa730ccfb
Uday <suravarapuuday160@gmail.com>
2026-03-22 22:51:39 +0530

    Create Brick_Breaker.cpp with border setup
    Initialized a 2D array for the game border.

---
+++ Projetcs/Brick_Breaker.cpp
@@ -0,0 +1,19 @@
+//Ongoing not yet completed 
+//As of today created a 2D array and initiliased it dor setting up the border 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char border[10][20];
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 20; j++)
+        {
+            border[i][j] = ' ';
+        }
+    }
+
+    return 0;
+}
