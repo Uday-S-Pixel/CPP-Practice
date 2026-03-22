e09f3de7035cb8aa3105d217f4263631c027223c
Uday <suravarapuuday160@gmail.com>
2026-03-22 15:39:16 +0530

    Create basic tic-tac-toe game structure
    This code currenly requires so many changes

---
+++ Projetcs/tic_tac_toe.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i,j;
+  char interface[3][3];
+
+  for ( i = 0; i < 3; i++)
+  {
+    
+    for ( j = 0; j < 3; j++)
+    {
+      interface[i][j] = ' ';
+    }
+  }
+
+   for ( i = 0; i < 3; i++)
+  {
+    
+    for ( j = 0; j < 3; j++)
+    {
+     cout << " " << interface[i][j] << " ";
+     cout << "|";
+    }
+    cout<<"\n.....";
+  }
+
+}
