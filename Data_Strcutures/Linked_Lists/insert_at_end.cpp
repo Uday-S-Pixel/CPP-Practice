0316fa5d9356250b0b007909a6bddf2911f8ed03
Uday <suravarapuuday160@gmail.com>
2026-04-21 21:48:27 +0530

    Implement insert at end for linked list

---
+++ Data_Strcutures/Linked_Lists/insert_at_end.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+  int data;
+  Node* next;
+};
+
+int main()
+{
+  Node* head = nullptr;
+
+  Node* first = new Node;
+  first->data = 10;
+  first->next = nullptr;
+  head = first;
+
+  Node* second = new Node;
+  second->data = 20;
+  second->next = nullptr;
+  first->next = second;
+
+  Node* third = new Node;
+  third->data = 30;
+  third->next = nullptr;
+  second->next = third;
+  
+  // Insert at end
+  Node* last = new Node;
+  last->data = 50;
+  last->next = nullptr;
+
+  Node* trav = head;
+
+  while(trav->next != nullptr)
+  {
+    trav = trav->next;
+  }
+
+  trav->next = last;   
+
+  // Display list
+  Node* temp = head;
+  while(temp != nullptr)
+  {
+    cout << temp->data << endl;
+    temp = temp->next;
+  }
+
+  return 0;
+}
