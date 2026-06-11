ae0ccb223112be0f09b811c59fda9268270f03c4
Uday <suravarapuuday160@gmail.com>
2026-06-11 22:54:58 +0530

    Add left-aligned right triangle pattern program
    This program prints a left-aligned right triangle pattern based on user input for the number of rows.

---
+++ Data_Strcutures/Patterns/2.left_alligned_right_triangle.cpp
@@ -0,0 +1,28 @@
+//Source code 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter no of rows: ";
+  cin >> n;
+  for (int i = 0; i < n ;i ++)
+  {
+    for (int j = 0; j <= i;j ++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+ return 0;
+}
+
+//Expected Output
+/*
+*
+**
+***
+****
+*****
+*/
